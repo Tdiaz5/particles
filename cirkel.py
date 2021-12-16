52b4e0cc97988ed8c26a935f771379159a0b756c
tdiaz5 <tobiasdiazdenhollander@live.nl>
2021-12-16 15:16:48 +0100

    wekr tniet!!!

--- cirkel.py
+++ cirkel.py
@@ -8,9 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-
+from numpy.core.numeric import _array_equal_dispatcher
 
 N = 2
+SCALE = 0.5
+A = 1
+B = 2
+NSTEPS = 100
 
 def produce_particles(n):
     """
@@ -45,23 +49,58 @@ def total_energy(xlist, ylist):
     total_energy /= 2
     return total_energy
 
-def move_particle(scale):
+def move_particle(scale, particle):
     """
-    Takes particle, moves random length in random direction
+    Takes particle, moves random length (< scale * r) in random direction,
+    returns new position.
     """
+    length = scale * random.random()
+    direction = 2 * math.pi * random.random()
+    x_move = length * math.cos(direction)
+    y_move = length * math.sin(direction)
+    x = particle[0]
+    y = particle[1]
+    x_new = x + x_move
+    y_new = y + y_move
+    return [x_new, y_new]
+
+def move_inside(scale, particle):
+    """Calls move_particle function until the particle is inside circle."""
+    position = move_particle(scale, particle)
+    while math.sqrt(position[0]**2 + position[1]**2) > 1:
+        position = move_particle(scale, particle)
+    return position
+
+def move_particles(scale, x_list, y_list):
+    """
+    Takes lists of x-coordinates and y-coordinates, moves all in random
+    direction but does not move outside the circle.
+    """
+    index = 0
+    while index < len(x_list):
+        particle = [x_list[index], y_list[index]]
+        particle = move_inside(scale, particle)
+        x_list[index] = particle[0]
+        y_list[index] = particle[1]
+        index += 1
+    return x_list, y_list
 
 def annealing_step(xlist, ylist, T):
     """Computes one step of the annealing algorithm"""
 
     # step 1: make move
-    xlist_new, ylist_new = move_particles(xlist, ylist)
-    
+    print(xlist)
+    xlist_new, ylist_new = move_particles(SCALE, xlist, ylist)
+    # print(xlist)
     # sample U
     U = random.random()
-
+    # print(xlist, xlist_new)
     # compute alpha
+    # print(xlist_new, ylist_new)
+    # print(xlist, ylist)
     h_new = total_energy(xlist_new, ylist_new)
     h = total_energy(xlist, ylist)
+    # print(h, h_new)
     alpha = min(np.exp((h_new - h) / T), 1)
     
     # determine which list to return
@@ -75,14 +114,20 @@ def annealing_algorithm(a, b, nsteps, xlist, ylist):
     for n in range(nsteps):
         T_n = (a) / (np.log(n + b))
         xlist, ylist = annealing_step(xlist, ylist, T_n)
-
     return xlist, ylist
 
 if __name__ == "__main__":
     xlist, ylist = produce_particles(N)
+    # print(total_energy([1, -1], [1, -1]))
+    # print(total_energy([0, 0], [1, -1]))
 
-    plt.xlim([-1, 1])
-    plt.ylim([-1, 1])
+    xlist, ylist = annealing_algorithm(A, B, NSTEPS, xlist, ylist)
+
+    # for i in range(100):
+        # xlist, ylist = move_particles(SCALE, xlist, ylist)
+
+    # plt.xlim([-1, 1])
+    # plt.ylim([-1, 1])
     
-    plt.plot(xlist, ylist, "ro")
-    plt.show()
+    # plt.plot(xlist, ylist, "ro")
+    # plt.show()
