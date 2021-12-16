6a743a5fa8a4cd1438ae2977c21e81ef34a58f33
tdiaz5 <tobiasdiazdenhollander@live.nl>
2021-12-16 14:03:14 +0100

    total energy calculation

--- cirkel.py
+++ cirkel.py
@@ -17,14 +17,33 @@ def produce_particles(n):
     Produces n particles at random positions in circle, i.e. gives random x and
     y as coordinates. Adds to list. Returns: list of n [x, y] lists.
     """
-    particle_list = []
-    while len(particle_list) < n:
+    xlist, ylist = [], []
+    while len(xlist) < n:
         x = random.random() * 2 - 1
         y = random.random() * 2 - 1
         r = math.sqrt(x**2 + y**2)
         if r < 1:
-            particle_list.append([x, y])
-    return particle_list
+            xlist.append(x)
+            ylist.append(y)
+    return xlist, ylist
+
+def calculate_total_energy(xlist, ylist):
+    """
+    Calculates the total system energy for a given xlist, ylist
+    """
+
+    total_energy = 0
+    N = len(xlist)
+
+    for i in range(N):
+        for j in range(N):
+            if i != j:
+                r_ij = ((xlist[i] - xlist[j]) ** 2 + (ylist[i] - ylist[j]) ** 2) ** (1 / 2)
+                total_energy += 1 / r_ij
+
+    # account for double counts (but doesn't really matter)
+    total_energy /= 2
+    return total_energy
 
 def move_particle(scale):
     """
@@ -33,15 +52,10 @@ def move_particle(scale):
 
 
 if __name__ == "__main__":
-    system = produce_particles(N)
-
-
+    xlist, ylist = produce_particles(N)
 
     plt.xlim([-1, 1])
     plt.ylim([-1, 1])
-    xlist, ylist = [], []
-    for particle in system:
-        xlist.append(particle[0])
-        ylist.append(particle[1])
+    
     plt.plot(xlist, ylist, "ro")
     plt.show()
